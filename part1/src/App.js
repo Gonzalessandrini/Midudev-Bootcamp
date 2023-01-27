8127510f88ffbf60e2ca18ca903f8b067d373e25
Densetsu <alessandrinigonzalo3@gmail.com>
2023-01-27 15:08:11 -0300

    first commit

---
+++ part1/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Mensaje from './Mensaje';
+
+const Descripcion= ()=>{
+  return <p>Esta es la app del curso fullstack bootcamp</p>
+}
+
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <Mensaje color='red' message= 'Estamos trabajando'/>
+      <Mensaje  color='green' message= 'En un curso'/>
+      <Mensaje  color='blue' message= 'De react xD'/>
+
+      <Descripcion/>
+
+      </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('Hola mundo')
+
+let  name= 'Gonzalo'
+const  lastName= 4
+
+const sumar= (n1,n2)=>{
+    console.log(n1)
+    console.log(n2)
+    return n1+n2
+}
+
+function restar(n1,n2)
+{
+    console.log(n1)
+    console.log(n2)
+    return n1-n2
+}
+
+
+const resultado= sumar(1,3)
+console.log(resultado)
+
+const resta= restar(5,3)
+console.log (resta)
